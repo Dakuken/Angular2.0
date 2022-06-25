806f7ea9327c92315d1f1921cccd27bfe981c4aa
BURNEL Justin <justin.burnel3@etu.univ-lorraine.fr>
2022-06-25 18:22:11 +0200

    project en scss

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-angular-app'),
+      dir: require('path').join(__dirname, './coverage/angular2.0'),
       subdir: '.',
       reporters: [
         { type: 'html' },
