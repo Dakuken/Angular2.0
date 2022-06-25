b38db8791d03cb81bb777a0398469ea6fe732c1d
BURNEL Justin <justin.burnel3@etu.univ-lorraine.fr>
2022-06-25 21:55:26 +0200

    en fait c'est le feu Angular wesh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from './service/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular2.0';
+export class AppComponent implements OnInit {
+  isAuth = false;
+
+  lastUpdate: Promise<Date> = new Promise((resolve, reject) => {
+    const date: Date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000);
+  });
+
+
+  appareils: any[] = []
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
+
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+
+  onEteindre() {
+    this.appareilService.switchOffAll();
+  }
 }
---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AppareilService } from '../service/appareil.service';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+  @Input() appareilName: string;
+  @Input() appareilStatus: string;
+  @Input() indexOfAppareil: number;
+
+  constructor(private appareilService: AppareilService) {
+    this.appareilName = "";
+    this.appareilStatus = "";
+    this.indexOfAppareil = 0
+  }
+
+  ngOnInit(): void {
+  }
+
+  getStatus() {
+    return this.appareilStatus;
+  }
+
+  getColor() {
+    if (this.appareilStatus === 'éteint') {
+      return "#fc0303"
+    }
+    return "#008a40"
+  }
+
+  onSwitchOn() {
+    this.appareilService.switchOnOne(this.indexOfAppareil);
+  }
+
+  onSwitchOff() {
+    this.appareilService.switchOffOne(this.indexOfAppareil);
+  }
+
+}
